858982ace9548eca362cc63d4c9ead0bf2100900
ashley_f_dsouza <ashley.f.dsouza@oracle.com>
2018-11-28 17:30:30 -0800

    Attempt 2: Navigation from Attempt 1 + Fragments. Need to add Fragment classes

--- app/src/main/java/com/example/ashleydsouza/takeyourmeds/HomePageActivity.java
+++ app/src/main/java/com/example/ashleydsouza/takeyourmeds/HomePageActivity.java
@@ -1,8 +1,20 @@
 package com.example.ashleydsouza.takeyourmeds;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -11,6 +23,10 @@ import java.util.Date;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +34,27 @@ public class HomePageActivity extends AppCompatActivity {
 
 //        Intent intent = getIntent();
 //        String email = intent.getStringExtra("email");
+
+        // Set a Toolbar to replace the ActionBar.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //email for the Navigation Tab under the Icon and App Name
+        navigationView = findViewById(R.id.navigation_drawer);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = headerView.findViewById(R.id.user_email);
+        navUsername.setText("ash@test.com");  //change this to email from intent
+
+
+        //Animation for the Hamburger icon to make sure it is clicked during navigation
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+
+        //setup the drawer view
+        setupDrawerContent(navigationView);
 
         TextView welcome = findViewById(R.id.welcome_view);
         String welcomeString = "Hi Ashley!";//getString(R.string.welcome_msg) + " " + email + "!";
@@ -29,6 +66,91 @@ public class HomePageActivity extends AppCompatActivity {
         welcomeString = welcomeString + "\n" + prescriptionForToday;
 
         welcome.setText(welcomeString);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * This is called when Navigation Drawer is selected
+     * @param navigationView
+     */
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
+    }
+
+    /**
+     * Sets the right fragment based on the user selection
+     * @param menuItem
+     */
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.add_prescription:
+                fragmentClass = AddPrescriptionFragment.class;
+                break;
+            case R.id.show_calender:
+                fragmentClass = ShowCalenderFragment.class;
+            case R.id.settings:
+                fragmentClass = SettingsFragment.class;
+            default:
+                fragmentClass = ShowCalenderFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        /**
+         * Highlight the selected item has been done by NavigationView,
+         * Set Action Bar title
+         * Close navigation drawer
+        **/
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
     }
 
     public String getPrescriptionsForToday(Date date, String userEmail) {
